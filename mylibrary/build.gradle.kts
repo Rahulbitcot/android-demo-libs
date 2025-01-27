e6cdd8bb12700955dbc0e75f63bbb94b0d6e3221
Rahulbitcot <rahulmukati@bitcot.com>
2025-01-27 11:44:59 +0530

    Update build.gradle.kts

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.library")
+    kotlin("android")
+    id("maven-publish") // Proper Kotlin DSL syntax
 }
 
 android {
@@ -9,7 +10,6 @@ android {
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -21,23 +21,48 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            consumerProguardFiles("proguard-rules.pro")
+            consumerProguardFiles("consumer-rules.pro")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.Rahulbitcot" // Replace with your GitHub username
+            artifactId = "android-demo-libs" // Replace with your repository name
+            version = "1.0" // Replace with version number
+            pom {
+                description.set("Sample Library for Android") // Replace with your library's description
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
 }
